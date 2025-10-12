9bfa66261e7d5b3caa06138eefb135dcb9ec397a
yoonseo23 <jysjdt03@gmail.com>
2025-10-13 04:28:54 +0900

    Chore : context provider 범위 변경 - 정렬 버튼을 포함하도록 변경

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,10 +1,16 @@
-import KakaoMap from '@/components/main/kakao-map'
+import { CourseCardList } from '../../components/main/course-card-list'
+import { MainHeaderBar } from '../../components/main/main-header-bar'
+import { CourseProvider } from '../../features/main/course-crud/context'
+import { getInitialCourse } from '../../features/main/course-crud/get-initial-course'
 
-export default function MainPage() {
+export default async function MainPage() {
+  const initialCourseData = await getInitialCourse()
   return (
     <main>
-      <h1>러닝 코스 지도</h1>
-      <KakaoMap />
+      <CourseProvider initialCourses={initialCourseData}>
+        <MainHeaderBar />
+        <CourseCardList />
+      </CourseProvider>
     </main>
   )
 }
