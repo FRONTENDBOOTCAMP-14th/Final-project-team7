cba49fceee6f9ccf8a295a57b9697fa42e728e3b
HanUL072 <dbstjrals125@naver.com>
2025-10-13 16:23:40 +0900

    Style : 프로필 편집 페이지 스타일 수정
    - w-full 추가로 가로가 유연히 변형되도록 수정

--- src/app/profile/profile-edit/layout.tsx
+++ src/app/profile/profile-edit/layout.tsx
@@ -5,7 +5,7 @@ import '@/styles/main.css'
 export default function ProfileEditLayout({ children }: PropsWithChildren) {
   return (
     <>
-      <main className="flex justify-center items-start min-h-screen py-10">
+      <main className="flex justify-center items-start w-full min-h-screen py-10">
         {/* 공용 상세페이지 네비바 */}
         {children}
       </main>
