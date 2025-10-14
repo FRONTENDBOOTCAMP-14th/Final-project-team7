acd47d8137dcbdd87be454365b79c3a228558f37
HanUL072 <dbstjrals125@naver.com>
2025-10-14 09:34:50 +0900

    Remove : html, body 중첩 코드 제거

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -3,12 +3,8 @@ import type { PropsWithChildren } from 'react'
 
 export default function ProfileLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="ko-KR">
-      <body>
-        <main className="flex justify-center items-start min-h-screen py-10">
-          {children}
-        </main>
-      </body>
-    </html>
+    <main className="flex justify-center items-start min-h-screen py-10">
+      {children}
+    </main>
   )
 }
--- src/app/profile/profile-edit/layout.tsx
+++ src/app/profile/profile-edit/layout.tsx
@@ -4,11 +4,9 @@ import '@/styles/main.css'
 
 export default function ProfileEditLayout({ children }: PropsWithChildren) {
   return (
-    <>
-      <main className="flex justify-center items-start w-full min-h-screen py-10">
-        {/* 공용 상세페이지 네비바 */}
-        {children}
-      </main>
-    </>
+    <main className="flex justify-center items-start w-full min-h-screen py-10">
+      {/* 공용 상세페이지 네비바 */}
+      {children}
+    </main>
   )
 }
