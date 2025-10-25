b95f296c0b9b31b24a5e6a605699da9064042f91
HanUL072 <dbstjrals125@naver.com>
2025-10-26 00:50:42 +0900

    Refactor : 러닝곡 페이지 경로 수정

--- src/components/profile/header-client.tsx
+++ src/components/profile/header-client.tsx
@@ -8,7 +8,7 @@ const PAGE_TITLES: Record<string, string> = {
   '/profile/profile-edit': '프로필 편집',
   '/profile/calendar': '러닝 캘린더',
   '/profile/friends': '친구 관리',
-  '/profile/music': '러닝곡 관리',
+  '/profile/running-music': '러닝곡 관리',
 }
 
 export default function HeaderClient() {
